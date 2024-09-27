ddcd8334cd1b8845ca5968e7eaed9852a960d949
Thomas Mathers <thomas.mathers@ideanotion.net>
2024-09-26 21:44:02 -0400

    added swagger

--- account-service/src/app.ts
+++ account-service/src/app.ts
@@ -4,6 +4,8 @@ import env from '../env';
 import AccountRepository from './repositories/accountRepository';
 import AuthService from './services/authService';
 import AccountService from './services/accountService';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
 
 const accountRepository = new AccountRepository();
 const accountService = new AccountService(accountRepository);
@@ -13,17 +15,57 @@ const authService = new AuthService(accountService);
 const app = express();
 
 app.use(bodyParser.json());
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(
+    swaggerJsdoc({
+      swaggerDefinition: {
+        openapi: '3.0.0',
+        info: { title: 'Message Service', version: '1.0.0' },
+      },
+      apis: ['src/**/*.ts'],
+    }),
+  ),
+);
 
+/**
+ * @openapi
+ * /accounts:
+ *   post:
+ *     description: Creates a new account.
+ *     responses:
+ *       201:
+ *         description: Returns the new account.
+ */
 app.post('/accounts', (req: Request, res: Response) => {
   const { statusCode, data } = accountService.createAccount(req.body);
   res.status(statusCode).json(data);
 });
 
+/**
+ * @openapi
+ * /accounts:
+ *   get:
+ *     description: Gets the list of accounts.
+ *     responses:
+ *       200:
+ *         description: Returns the list of accounts.
+ */
 app.get('/accounts', (req: Request, res: Response) => {
   const { statusCode, data } = accountService.getAccounts();
   res.status(statusCode).json(data);
 });
 
+/**
+ * @openapi
+ * /auth/login:
+ *   get:
+ *     description: Log in.
+ *     responses:
+ *       200:
+ *         description: Returns a JWT.
+ */
 app.post('/auth/login', (req: Request, res: Response) => {
   const { statusCode, data } = authService.login(req.body);
   res.status(statusCode).json(data);
--- message-service/src/app.ts
+++ message-service/src/app.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
 import expressWs from 'express-ws';
-import env from '../env';
 import bodyParser from 'body-parser';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import env from '../env';
 import MessageRepository from './repositories/messageRepository';
 import MessageService from './services/messageService';
 import jwtMiddleware from './middlewares/jwtMiddleware';
@@ -14,6 +16,19 @@ const messageRepository = new MessageRepository();
 const messageService = new MessageService(messageRepository, wss);
 
 app.use(bodyParser.json());
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(
+    swaggerJsdoc({
+      swaggerDefinition: {
+        openapi: '3.0.0',
+        info: { title: 'Message Service', version: '1.0.0' },
+      },
+      apis: ['src/**/*.ts'],
+    }),
+  ),
+);
 app.use(jwtMiddleware);
 
 app.ws('/realtime', (ws, req) => {
@@ -22,6 +37,17 @@ app.ws('/realtime', (ws, req) => {
   });
 });
 
+/**
+ * @openapi
+ * /messages:
+ *   get:
+ *     description: Gets the list of messages.
+ *     responses:
+ *       200:
+ *         description: Returns the list of messages.
+ *       401:
+ *         description: Unauthorized.
+ */
 app.get('/messages', (req: Request, res: Response) => {
   const { statusCode, data } = messageService.getMessages();
   res.status(statusCode).json(data);
