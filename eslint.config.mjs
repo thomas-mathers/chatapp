a54c04a71109b08cd6a2b68785ee929d2db955e3
Thomas Mathers <thomas.mathers@ideanotion.net>
2024-10-14 00:06:54 -0400

    changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'] },
   { languageOptions: { globals: globals.browser } },
   js.configs.recommended,
   ...tseslint.configs.recommended,
